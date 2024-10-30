63b449171d51701223a5fca4c28e538b89c88ac5
emreyilmazb <emreyilmazba@gmail.com>
2024-10-30 10:59:10 +0300

    model form içerisine validator ile cv doğrulama alanı eklendi.

--- form/views.py
+++ form/views.py
@@ -13,18 +13,8 @@ def job_application_form(request):
     if request.method == 'POST':
         form = ApplyModelForm(request.POST, request.FILES)
         if form.is_valid():
-            uploaded_file = request.FILES['cv']  # `cv` dosya alanı ismini doğrulayın.
-
-            # Validator sınıfını kullanarak doğrulama
-            validator = FileUploadValidator(allowed_extensions=['png', 'jpg', 'jpeg'], max_size=2 * 1024 * 1024)  # Örnek: PDF ve 2 MB sınırı
-            try:
-                validator(uploaded_file)
-                form.save()
-                return redirect('index')
-            except ValidationError as e:
-                form.add_error('cv', str(e))
-        else:
-            pass
+            form.save()
+            return redirect('index')
     else:
         form = ApplyModelForm()
 
